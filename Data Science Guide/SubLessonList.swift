baa395544b7ac002492fb0bb4717aa843622ecfc
Patrick Long <patrick.long@gmail.com>
2019-08-27 21:31:52 -0400

    initial setup - succeed

---
+++ Data Science Guide/SubLessonList.swift
@@ -0,0 +1,23 @@
+//
+//  SubLessonList.swift
+//  Data Science Guide
+//
+//  Created by patrick long on 8/27/19.
+//  Copyright © 2019 patrick long. All rights reserved.
+//
+
+import SwiftUI
+
+struct SubLessonList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#if DEBUG
+struct SubLessonList_Previews: PreviewProvider {
+    static var previews: some View {
+        SubLessonList()
+    }
+}
+#endif
